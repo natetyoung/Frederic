bd79b6abab81c438b3f53d7aab1a424d51bee80e
natetyoung <natetyoung42@gmail.com>
2015-06-09 18:28:32 -0700

    Added DoCommand interface
    This is the interface to be implemented in order to create a new action
    to be considered when the user issues the "do something" command

---
+++ DoCommand.java
@@ -0,0 +1,18 @@
+package src;
+/**
+ * A simple interface to represent a possible action to be taken when the user enters "do something"
+ * @author Nate Young
+ *
+ */
+public interface DoCommand {
+	/**
+	 * Performs the action this <code>DoCommand</code> represents.
+	 * <br> Implementing classes should write the behavior of the intended action into this method.<br>
+	 * For example, if one possible action to be taken when the user enters "do something" is printing the 
+	 * current system nanotime to the <code>FancyConsole</code>, this method would contain <br>
+	 * <code>f.out.println(System.nanoTime());</code>
+	 * 
+	 * @param f The <code>Frederic</code> that called the method (for reading/printing purposes)
+	 */
+	public void execute(Frederic f);
+}
